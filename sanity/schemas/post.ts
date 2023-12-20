c6d9d31b33868707362ad4e40eb320f49e4da3ec
shouravrahman <shouravrahman006@gmail.com>
2023-12-20 23:12:23 +0600

    schemas

---
+++ sanity/schemas/post.ts
@@ -0,0 +1,65 @@
+import { Rule } from "sanity";
+
+export const post = {
+
+    name: "post",
+    title: "post",
+    type: "document",
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            validation: (Rule: Rule) => Rule.required().error("Required")
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'string',
+            options: {
+
+                source: "title"
+
+            },
+            validation: (Rule: Rule) => Rule.required().error("Required")
+        },
+        {
+
+            name: 'publishedAt',
+            title: 'Published At',
+            type: 'datetime',
+            initialValue: () => new Date().toISOString()
+        },
+        {
+
+            name: 'excerpt',
+            title: 'Excerpt',
+            type: 'text',
+            validation: (Rule: Rule) => Rule.required().max(200).error("Max 200 characters")
+
+        },
+        {
+
+            name: 'body',
+            title: 'Body',
+            type: 'array',
+            of: [
+
+                { type: 'block' },
+                {
+                    type: "image",
+                    fields: [{ type: 'text', name: 'alt', title: 'alt' }]
+                }
+            ]
+
+        },
+        {
+
+            name: 'tags',
+            title: 'Tags',
+            type: "array",
+            of: [{ type: 'reference', to: [{ type: 'tag' }] }]
+        }
+    ]
+
+}
