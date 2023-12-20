3bb3c65c4102f532047b2328222c26e8c4def5b4
shouravrahman <shouravrahman006@gmail.com>
2023-12-20 23:12:45 +0600

    imported post schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import {post} from "@/sanity/schemas/post";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [post],
 }
